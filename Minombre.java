09ac9d967b499c3350d28a1148c04cee0081215d
lenovo <renanjesuskuchan12@gmail.com>
2024-09-23 23:42:40 -0600

    Prueba1

---
+++ Minombre.java
@@ -0,0 +1,10 @@
+package nombres;
+
+public class Minombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" Mi nombre es Renan Jesus Ku Chan");
+	}
+
+}
